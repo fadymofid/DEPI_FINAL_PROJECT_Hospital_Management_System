9a38f06f3821a5b2890eaac0852d7dc4d6a4330f
fady <108768356+fadymofid@users.noreply.github.com>
2024-10-04 06:51:18 +0300

    db update

--- hms.sql
+++ hms.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Sep 28, 2024 at 06:27 PM
+-- Generation Time: Oct 04, 2024 at 05:41 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -47,14 +47,10 @@ CREATE TABLE `appointments` (
 --
 
 INSERT INTO `appointments` (`id`, `name`, `email`, `phone`, `doctor_id`, `speciality`, `date`, `message`, `status`, `user_id`, `created_at`, `updated_at`) VALUES
-(1, 'فادي ميشيل', 'fadymichel134@gmail.com', '01228595410', 1, 'general_health', '2024-09-29', NULL, 'Approved', '1', '2024-09-28 10:59:14', '2024-09-28 11:04:37'),
-(3, 'ali', 'ali@gmail.com', '01278816303', 4, 'dental', '2024-09-29', NULL, 'Approved', '1', '2024-09-28 11:00:00', '2024-09-28 11:04:49'),
-(4, 'khaled', 'khaled@gmail.com', '0127234234', 6, 'dental', '2024-09-29', NULL, 'Approved', '1', '2024-09-28 11:00:24', '2024-09-28 11:04:40'),
-(5, 'Abdo', 'abdo@gmail.com', '01228595410', 5, 'neurology', '2024-09-30', NULL, 'In Progress', '1', '2024-09-28 11:00:52', '2024-09-28 11:00:52'),
-(6, 'pop', 'pop@gmail.com', '01223274286', 3, 'orthopaedics', '2024-09-30', NULL, 'Approved', '1', '2024-09-28 11:01:17', '2024-09-28 11:04:44'),
-(7, 'fadyy', 'fadymichel134@gmail.com', '01223274286', 2, 'cardiology', '2024-10-05', NULL, 'In Progress', NULL, '2024-09-28 12:30:24', '2024-09-28 12:30:24'),
-(8, 'ahmed', 'fadymichel134@gmail.com', '01228595410', 4, 'dental', '2024-09-29', NULL, 'In Progress', NULL, '2024-09-28 12:30:48', '2024-09-28 12:30:48'),
-(9, 'fady mofid', 'fadymichel134@gmail.com', '01223274286', 4, 'dental', '2024-09-29', NULL, 'In Progress', NULL, '2024-09-28 12:31:04', '2024-09-28 12:31:04');
+(4, 'fady mofid', 'fadymichel134@gmail.com', '01223274286', 1, 'general_health', '2024-10-05', NULL, 'Approved', '1', '2024-10-03 23:02:09', '2024-10-03 23:02:25'),
+(5, 'sdfadf', 'fadymichel134@gmail.com', '01228595410', 6, 'dental', '2024-11-01', NULL, 'Approved', '1', '2024-10-03 23:14:22', '2024-10-03 23:14:39'),
+(6, 'fady mofid', 'fadymichel134@gmail.com', '01228595410', 4, 'orthopaedics', '2024-10-19', NULL, 'Approved', '1', '2024-10-03 23:28:43', '2024-10-03 23:28:59'),
+(7, 'fady mofid', 'fadymichel134@gmail.com', '01228595410', 6, 'dental', '2024-10-10', NULL, 'Approved', '1', '2024-10-03 23:33:24', '2024-10-03 23:33:41');
 
 -- --------------------------------------------------------
 
@@ -78,12 +74,12 @@ CREATE TABLE `doctors` (
 --
 
 INSERT INTO `doctors` (`id`, `name`, `phone`, `speciality`, `consultation_fee`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Dr. Hoda Azmy', '01223274286', 'general_health', 100, '1727529727.jpeg', '2024-09-28 10:22:07', '2024-09-28 10:22:07'),
-(2, 'Dr. Matt Damon', '01223274286', 'cardiology', 300, '1727529893.jpg', '2024-09-28 10:24:53', '2024-09-28 10:24:53'),
-(3, 'DR. Amr Zaky', '01278816303', 'orthopaedics', 360, '1727529936.jpeg', '2024-09-28 10:25:36', '2024-09-28 10:25:36'),
-(4, 'DR. Nada Essam', '01278816303', 'dental', 400, '1727530073.jpeg', '2024-09-28 10:27:53', '2024-09-28 10:27:53'),
-(5, 'Dr. Amin Ayman', '01223274286', 'neurology', 120, '1727530811.jpg', '2024-09-28 10:40:11', '2024-09-28 10:40:11'),
-(6, 'Dr. Lyla Elwy', '01278816303', 'dental', 150, '1727530988.jpg', '2024-09-28 10:43:08', '2024-09-28 10:43:08');
+(1, 'Dr. Hoda Azmy', '01228595410', 'general_health', 100, '1728003681.jpeg', '2024-10-03 22:01:21', '2024-10-03 22:01:21'),
+(2, 'Dr. Matt Damon', '01223274286', 'cardiology', 120, '1728003727.jpg', '2024-10-03 22:02:07', '2024-10-03 22:02:07'),
+(3, 'DR. Nada Essam', '01223274286', 'dental', 180, '1728003766.jpeg', '2024-10-03 22:02:46', '2024-10-03 22:02:46'),
+(4, 'Dr. Amin Ayman', '01278816303', 'orthopaedics', 200, '1728003789.jpg', '2024-10-03 22:03:09', '2024-10-03 22:03:09'),
+(5, 'Dr. Karim Gamil', '01228595410', 'neurology', 250, '1728003851.jpg', '2024-10-03 22:04:11', '2024-10-03 22:04:11'),
+(6, 'Dr. Mariam Hany', '01228595410', 'dental', 220, '1728003902.jpeg', '2024-10-03 22:05:02', '2024-10-03 22:05:02');
 
 -- --------------------------------------------------------
 
@@ -123,12 +119,13 @@ CREATE TABLE `medicines` (
 --
 
 INSERT INTO `medicines` (`id`, `name`, `image`, `description`, `category`, `price`, `created_at`, `updated_at`) VALUES
-(1, 'Codral', '1727531075.jpg', 'For flu', 'antiseptics', 20.00, '2024-09-28 10:44:36', '2024-09-28 13:02:42'),
-(2, 'Panadol Extra', '1727531108.jpg', 'For flu', 'Painkillers', 10.00, '2024-09-28 10:45:08', '2024-09-28 10:45:08'),
-(3, 'Gummy Multivitamins', '1727531188.jpg', 'For kids', 'vitamins', 120.00, '2024-09-28 10:46:28', '2024-09-28 10:46:28'),
-(4, 'Congestal', '1727531261.png', 'For Headaches', 'antibiotics', 120.00, '2024-09-28 10:47:41', '2024-09-28 10:50:31'),
-(5, 'Centrum', '1727531547.jpg', 'Made in USA', 'supplements', 89.00, '2024-09-28 10:52:27', '2024-09-28 10:52:27'),
-(6, 'Xoraxon', '1727531637.jpg', 'For kids', 'antiseptics', 90.00, '2024-09-28 10:53:57', '2024-09-28 10:53:57');
+(1, 'Panadol Extra', '1728012711.jpg', 'For Flu', 'Painkillers', 10.00, '2024-10-04 00:31:52', '2024-10-04 00:31:52'),
+(2, 'Codral', '1728012754.jpg', 'For flu', 'antibiotics', 15.00, '2024-10-04 00:32:34', '2024-10-04 00:32:34'),
+(3, 'Limitless Vitamin', '1728012866.jpg', 'made in USA', 'vitamins', 40.00, '2024-10-04 00:34:27', '2024-10-04 00:34:27'),
+(4, 'Xoraxon', '1728012909.jpg', 'made in UK', 'antibiotics', 89.99, '2024-10-04 00:35:09', '2024-10-04 00:35:09'),
+(5, 'Centrum', '1728012976.jpg', 'for general health', 'supplements', 500.00, '2024-10-04 00:36:16', '2024-10-04 00:36:16'),
+(6, 'Congestal', '1728013165.png', NULL, 'antiseptics', 30.00, '2024-10-04 00:39:25', '2024-10-04 00:39:25'),
+(7, 'Mederma', '1728013222.jpg', NULL, 'antibiotics', 100.00, '2024-10-04 00:40:22', '2024-10-04 00:40:22');
 
 -- --------------------------------------------------------
 
@@ -147,15 +144,39 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_100000_create_password_resets_table', 1),
-(2, '2019_08_19_000000_create_failed_jobs_table', 1),
-(3, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(4, '2024_09_11_231900_create_sessions_table', 1),
-(5, '2024_09_12_180054_create_users_table', 1),
-(6, '2024_09_16_032707_create_doctors_table', 1),
-(7, '2024_09_17_014314_create_appointments_table', 1),
-(8, '2024_09_19_000909_create_medicines_table', 1),
-(9, '2024_10_20_200000_add_two_factor_columns_to_users_table', 1);
+(11, '2014_10_12_100000_create_password_resets_table', 1),
+(12, '2019_08_19_000000_create_failed_jobs_table', 1),
+(13, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(14, '2024_09_11_231900_create_sessions_table', 1),
+(15, '2024_09_12_180054_create_users_table', 1),
+(16, '2024_09_16_032707_create_doctors_table', 1),
+(17, '2024_09_17_014314_create_appointments_table', 1),
+(18, '2024_09_19_000909_create_medicines_table', 1),
+(19, '2024_10_04_000308_create_notifications_table', 1),
+(20, '2024_10_20_200000_add_two_factor_columns_to_users_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notifications`
+--
+
+CREATE TABLE `notifications` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `appointment_id` bigint(20) UNSIGNED DEFAULT NULL,
+  `message` text NOT NULL,
+  `is_read` tinyint(1) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `user_id`, `appointment_id`, `message`, `is_read`, `created_at`, `updated_at`) VALUES
+(6, 1, 7, 'تمت الموافقة على موعدك في 2024-10-10 مع Dr. Mariam Hany.', 1, '2024-10-03 23:33:41', '2024-10-03 23:35:11');
 
 -- --------------------------------------------------------
 
@@ -208,7 +229,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('Y7yNOY7wgg3OHXZ1g0T3ME7MLCNWXwFlMAiWRxhT', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoid0U4cjVFSkFhQldRYW11MVNRbDYwTUg1ajMwWjhZN3VKT05UY1FGQyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDU6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9hcGkvZG9jdG9yLWFwcG9pbnRtZW50cyI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7czo2OiJsb2NhbGUiO3M6MjoiZW4iO30=', 1727540735);
+('P507VWC9FehWPLFiVNpQ7KHfvh3vED7L0jtmWoT3', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoicGJ1emdyOXo1R2FRc0pUbzN2Z2UzY3poVDFLTWlXbERadzRSUDVJTCI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjU6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9tZWQiO31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1728013248);
 
 -- --------------------------------------------------------
 
@@ -227,7 +248,7 @@ CREATE TABLE `users` (
   `phone` varchar(255) DEFAULT NULL,
   `address` varchar(255) DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `type` varchar(255) NOT NULL DEFAULT 'admin',
+  `type` varchar(255) NOT NULL DEFAULT 'client',
   `remember_token` varchar(100) DEFAULT NULL,
   `current_team_id` bigint(20) UNSIGNED DEFAULT NULL,
   `profile_photo_path` varchar(2048) DEFAULT NULL,
@@ -240,8 +261,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `two_factor_confirmed_at`, `phone`, `address`, `email_verified_at`, `type`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(1, 'Khaled', 'khaled@gmail.com', '$2y$10$ZcBWPcxKgvJHgoseu.Zg5Oo9Vw2UOPYa3hj0c7Swx1lPPMTAy7AIC', NULL, NULL, NULL, '01228595410', '122 mohamed el imbaby', NULL, 'client', NULL, NULL, NULL, '2024-09-28 10:12:28', '2024-09-28 10:56:55'),
-(2, 'admin', 'admin@gmail.com', '$2y$10$1ma2hDnLZ7gg03U/fiV3D.G4xxat5WeqIS0bssrtkI/y0Sx1cXKvu', NULL, NULL, NULL, '01223274286', '122 st mohamed el imbaby', NULL, 'admin', NULL, NULL, NULL, '2024-09-28 10:14:32', '2024-09-28 10:14:32');
+(1, 'khaled', 'khaled@gmail.com', '$2y$10$pmV/6WFsJXD7QhyYuwgGXOzkeTjttJTSZ2sy77EeRPs375.Fx2ZZe', NULL, NULL, NULL, '01228595410', '122 mohamed el imbaby', NULL, 'client', NULL, NULL, NULL, '2024-10-03 21:59:32', '2024-10-03 21:59:32'),
+(2, 'admin', 'admin@gmail.com', '$2y$10$nODrpzIP60MkNlexpCxsEegcYZ7B3h6YPHy4EdBhRRKS40IrC6l4K', NULL, NULL, NULL, '01223274286', '122 mohamed el imbaby', NULL, 'admin', NULL, NULL, NULL, '2024-10-03 22:00:02', '2024-10-03 22:00:02');
 
 --
 -- Indexes for dumped tables
@@ -280,6 +301,14 @@ ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `notifications_user_id_foreign` (`user_id`),
+  ADD KEY `notifications_appointment_id_foreign` (`appointment_id`);
+
+--
 -- Indexes for table `password_resets`
 --
 ALTER TABLE `password_resets`
@@ -316,7 +345,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `doctors`
@@ -334,13 +363,19 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `medicines`
 --
 ALTER TABLE `medicines`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `notifications`
+--
+ALTER TABLE `notifications`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -352,7 +387,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
@@ -363,6 +398,13 @@ ALTER TABLE `users`
 --
 ALTER TABLE `appointments`
   ADD CONSTRAINT `appointments_doctor_id_foreign` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `notifications`
+--
+ALTER TABLE `notifications`
+  ADD CONSTRAINT `notifications_appointment_id_foreign` FOREIGN KEY (`appointment_id`) REFERENCES `appointments` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `notifications_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
